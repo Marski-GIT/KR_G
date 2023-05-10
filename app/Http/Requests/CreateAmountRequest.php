b997e65866f5d057a6c4e44878abeebb15d0f6d2
Marski <komar_16@o2.pl>
2023-05-10 15:02:52 +0200

    Added Rules only EUR USD GBP currency

--- app/Http/Requests/CreateAmountRequest.php
+++ app/Http/Requests/CreateAmountRequest.php
@@ -17,15 +17,15 @@ class CreateAmountRequest extends FormRequest
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * @description You need to edit the regular expression to extend the currencies.
      * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            '*.currency' => 'required|string|max:3',
-            '*.date'     => 'date_format:Y-m-d',
-            '*.amount'   => 'required|numeric',
+            '*.currency' => ['required', 'regex:/(EUR)|(USD)|(GBP)|(eur)|(usd)|(gbp)/'],
+            '*.date'     => ['date_format:Y-m-d'],
+            '*.amount'   => ['required', 'numeric', 'gt:0'],
         ];
     }
 
@@ -33,10 +33,10 @@ class CreateAmountRequest extends FormRequest
     {
         return [
             '*.currency.required' => 'Currency name is required.',
-            '*.currency.string'   => 'The currency name must be a string.',
-            '*.currency.max'      => 'The maximum number of characters is max: :max',
+            '*.currency.regex'    => 'Only EUR, USD, GBP currencies.',
             '*.amount.required'   => 'Value is required.',
             '*.amount.numeric'    => 'The value must be a number.',
+            '*.amount.gt'         => 'The value must be positiver.',
             '*.date'              => 'Invalid date format. Date in Y-m-d format.'
         ];
     }
